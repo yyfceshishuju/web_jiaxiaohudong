3d317994610ceb006586d199ead3d3d7a113b714
liuchaofan <liuchaofan@xiaomi.com>
2018-10-31 20:57:07 +0800

    Merge branch 'master' of https://github.com/yyfceshishuju/web_jiaxiaohudong
    # Conflicts:
    #	src/main/java/com/jiaxiaohudong/controller/ImageController.java
    #	src/main/webapp/WEB-INF/views/class.jsp
    #	target/helloworld/META-INF/MANIFEST.MF

--- src/main/java/com/jiaxiaohudong/controller/StudentController.java
+++ src/main/java/com/jiaxiaohudong/controller/StudentController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,12 +26,15 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
     @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
     public R getStudents(HttpSession session ) throws RuntimeException {
 
         CommonUser user = (CommonUser) session.getAttribute("userinfo");
         List<CommonStudent> students = studentService.getStudents(user.getId());
+        Map<String, Object> result = new HashMap<String, Object>();
         if (students !=null){
-            final String result = JsonUtil.toJson(students);
+            result.put("data", students);
+            result.put("code", 0);
             return R.ok(result);
         }else{
             return R.error("没有查询结果");
@@ -38,6 +42,7 @@ public class StudentController {
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
     public R searchStudents(HttpSession session, @RequestParam(value = "name") String name ) throws RuntimeException {
         CommonUser user = (CommonUser) session.getAttribute("userinfo");
 
@@ -45,8 +50,10 @@ public class StudentController {
         commonStudent.setTid(user.getId());
         commonStudent.setName(name);
         List<CommonStudent> students = studentService.searchStudents(commonStudent);
+        Map<String, Object> result = new HashMap<String, Object>();
         if (students !=null){
-            final String result = JsonUtil.toJson(students);
+            result.put("data", students);
+            result.put("code", 0);
             return R.ok(result);
         }else{
             return R.error("没有查询结果");
